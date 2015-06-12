e833351576e0552d822c27aed5b1db6cbe7e5a66
Marcel Jackwerth <marceljackwerth@gmail.com>
2015-06-12 12:00:34 -0700

    Update wwdcdl.rb

--- wwdcdl.rb
+++ wwdcdl.rb
@@ -1,7 +1,7 @@
 class Wwdcdl < Formula
   desc "Tool do download WWDC videos and slides"
   homepage "https://github.com/sirlantis/wwdcdl"
-  url "https://github.com/sirlantis/wwdcdl/releases/download/2.0/wwdcdl-darwin.tar.gz"
+  url "https://github.com/sirlantis/wwdcdl/releases/download/2.1/wwdcdl-darwin.tar.gz"
   sha256 "ca6be88e09df04bd0930a0019639dadf7ccca8107482f08b61f39e8b580c2ccd"
   version "2.1"
 
